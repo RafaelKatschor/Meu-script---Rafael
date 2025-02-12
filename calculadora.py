a27e67acae591fc293060c11d540e093e060a582
Rafael Bonatto Katschor <rafael.bonatto1002@gmail.com>
2025-02-12 16:40:17 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,30 @@
+nome = input('Qual é o seu nome: ')
+
+print ('Seja bem vindo(a)', nome)
+
+
+
+
+num1 = float(input('Digite um número: '))
+num2 = float(input('Digite outro número: '))
+soma= num1 + num2
+subtraçao = num1 - num2
+multiplicaçao = num1 * num2
+divisao = num1 / num2
+divisao_inteira = num1 // num2
+resto = num1 % num2
+potencia = num1**2
+potencia2 = num2**2
+raiz = num1**(1/2)
+raiz2= num2**(1/2)
+raiz_cubica = num1**(1/3)
+raiz_cubica2 = num2**(1/3)
+print ('O valor da soma é {}, o valor da subtração é {}'.format(soma,subtraçao))
+print ('O valor da multiplicação é {}, o valor da divisão é {}'.format(multiplicaçao,divisao))
+print ('O valor da divisão inteira é {}, o valor do resto é {}'.format(divisao_inteira,resto))
+print ('O valor da potência do primeiro número é {}'.format(potencia))
+print ('O valor da potência do segundo número é {}'.format(potencia2))
+print ('O valor da raiz quadrada do primeiro número é {}'.format(raiz))
+print ('O valor da raiz quadrada do segundo número é {}'.format(raiz2))
+print('O valor da raiz cubica do primeiro número é {}'.format(raiz_cubica))
+print('O valor da raiz cubica do segundo número é {}'.format(raiz_cubica2))
